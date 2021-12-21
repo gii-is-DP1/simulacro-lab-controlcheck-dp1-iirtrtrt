40d20685971bb8a34e9623f27e4c04871cb50f80
iirtrtrt <iirtrtrt@gmail.com>
2021-12-21 12:30:31 +0100

    test

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -3,11 +3,28 @@ package org.springframework.samples.petclinic.product;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Min;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "Products")
 public class Product {
+    @Id
     Integer id;
+
+	@NotEmpty
+    @Size(min=3, max=50)
     String name;
+
+	@NotEmpty
+    @Min(value = 0)
     double price;
+    
+    @ManyToOne(targetEntity=ProductType.class, fetch=FetchType.LAZY)
+	@JoinColumn(name="productType_id")
     ProductType productType;
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -3,9 +3,26 @@ package org.springframework.samples.petclinic.product;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Min;
+import java.util.List;
+import java.util.ArrayList;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "ProductTypes")
 public class ProductType {
+    @Id
     Integer id;
+
+    @NotEmpty
+    @Size(min=3, max=50)
+    @Column(unique = true)
     String name;
+
+    @OneToMany(mappedBy = "products") 
+    private List<Product> products = new ArrayList<>();
 }
